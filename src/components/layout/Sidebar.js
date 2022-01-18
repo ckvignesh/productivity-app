567985eff8da58297d69a3cffede7345239f4b2f
ckvignesh <vigneshckprakash@gmail.com>
2022-01-18 12:21:20 +0530

    side bar WIP

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,3 +1,42 @@
 import React from "react";
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from "react-icons/fa";
 
-export const Sidebar = () => <p>"The Sidebar"</p>;
+export const Sidebar = () => (
+  <div className="sidebar" data-testid="sidebar">
+    <ul className="sidebar__generic">
+      <li>
+        <span>
+          <FaInbox />
+        </span>
+        <span>Inbox</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendar />
+        </span>
+        <span>Today</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendarAlt />
+        </span>
+        <span>Next 7 days</span>
+      </li>
+    </ul>
+    <div className="sidebar__middle">
+      <span>
+        <FaChevronDown />
+      </span>
+      <span>
+        <h2>Projects</h2>
+      </span>
+    </div>
+    <ul className="sidebar__projects">Projects are here</ul>
+    //Add Project Component comes here//
+  </div>
+);
