345fc7b9e03d87c45a207453a8141205a5e3e13e
ckvignesh <vigneshckprakash@gmail.com>
2022-01-18 12:33:43 +0530

    firebase js file added

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectID: "",
+  storageBucket: "",
+  messageSenderId: "",
+  apiId: "",
+});
+
+export { firebaseConfig as firebase };
