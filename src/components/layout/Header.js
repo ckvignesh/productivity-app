c19cee56da121995928d40e3593b9d65371fb92d
ckvignesh <vigneshckprakash@gmail.com>
2022-01-18 11:42:35 +0530

    test header created

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,3 +1,19 @@
 import React from "react";
 
-export const Header = () => <p> "Header" </p>;
+export const Header = () => {
+  return (
+    <header className="header" data-testid="header">
+      <nav>
+        <div className="logo">
+          <img src="/test.jpg" alt="appLogo" />
+        </div>
+        <div className="settings">
+          <ul>
+            <li>Test 1</li>
+            <li>Test 2</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
