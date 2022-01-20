9688fdb9d182bb1d15cf4397b6ae02dc8f679557
ckvignesh <vigneshckprakash@gmail.com>
2022-01-20 20:10:41 +0530

    firebase credentials

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectID: "",
-  storageBucket: "",
-  messageSenderId: "",
-  apiId: "",
+  apiKey: "AIzaSyB4BmUOeostv8Jgq_keFJTUS6i38NWWt9w",
+  authDomain: "productivity-app-vignesh-84ae5.firebaseapp.com",
+  databaseURL:
+    "https://productivity-app-vignesh-84ae5-default-rtdb.firebaseio.com",
+  projectId: "productivity-app-vignesh-84ae5",
+  storageBucket: "productivity-app-vignesh-84ae5.appspot.com",
+  messagingSenderId: "740947454569",
+  appId: "1:740947454569:web:4e2ba414b3ac36b15a90f8",
 });
 
 export { firebaseConfig as firebase };
