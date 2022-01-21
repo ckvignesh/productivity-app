31036af779f88776d9262d7e2ee05b525d9d494c
ckvignesh <vigneshckprakash@gmail.com>
2022-01-21 23:22:40 +0530

    hooks added

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,14 @@
 import { useState, useEffect } from "react";
 import { firebase } from "../firebase";
+
+const collatedTasks = () => {};
+export const useTasks = (selectedProject) => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    let unsubscribe = firebase
+      .firestore()
+      .collection("tasks")
+      .where("userId", "==", "1");
+  });
+};
