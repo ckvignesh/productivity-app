a63e162f0c946e29d80bbfd2ae513a2e107cbf61
ckvignesh <vigneshckprakash@gmail.com>
2022-01-22 08:14:28 +0530

    hooks file added

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { firebase } from "../firebase";
 
-const collatedTasks = () => {};
+const collatedTasksExist = () => {};
 export const useTasks = (selectedProject) => {
   const [tasks, setTasks] = useState([]);
 
@@ -10,5 +10,22 @@ export const useTasks = (selectedProject) => {
       .firestore()
       .collection("tasks")
       .where("userId", "==", "1");
-  });
+
+    unsubscribe =
+      selectedProject && !collatedTasksExist(selectedProject)
+        ? (unsubscribe = unsubscribe.where(
+            "projectId",
+            "==",
+            "selectedProject"
+          ))
+        : selectedProject === "TODAY"
+        ? (unsubscribe = unsubscribe.where(
+            "date",
+            "==",
+            moment().format("DD/MM/YYY")
+          ))
+        : selectedProject === "INBOX" || selectedProject === 0
+        ? (unsubscribe = unsubscribe.where("date", "==", ""))
+        : unsubscribe;
+  }, [selectedProject]);
 };
